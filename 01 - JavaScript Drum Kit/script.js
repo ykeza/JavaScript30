7d2889328ccd2bc5ac3d17cc433e717f3aa94e39
ykeza <yannickkeza@gmail.com>
2022-09-02 13:24:58 -0400

    Switched the event listener from 'keydown' to 'keyup' to prevent to infinity play loop when key is held

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,11 +1,16 @@
+let stopPlay = () => {
+
+}
+
 let playAudio = e => {
     let eCode = e.key.toUpperCase().charCodeAt();
     let audio = document.querySelector(`audio[data-key="${eCode}"]`);
     if (audio) {
+        audio.currentTime = 0;
         audio.play();
     } else {
         return;
     }
 }
 
-window.addEventListener('keydown', playAudio);
+window.addEventListener('keyup', playAudio);

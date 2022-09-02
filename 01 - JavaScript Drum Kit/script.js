203cb166fa6f0ab3d58bec0c67efef7ec59a639c
ykeza <yannickkeza@gmail.com>
2022-09-02 14:27:43 -0400

    Automated the 'playing' class toggle to last 0.1s, allowing for the keyup transition

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,16 +1,16 @@
-let stopPlay = () => {
-
-}
-
 let playAudio = e => {
     let eCode = e.key.toUpperCase().charCodeAt();
+    let eKey = document.querySelector(`div[data-key="${eCode}"]`);
     let audio = document.querySelector(`audio[data-key="${eCode}"]`);
     if (audio) {
+        eKey.setAttribute('class', 'key playing');
         audio.currentTime = 0;
         audio.play();
+        setTimeout(function() {
+            eKey.setAttribute('class', 'key');
+        }, 100);
     } else {
         return;
     }
 }
-
 window.addEventListener('keyup', playAudio);
